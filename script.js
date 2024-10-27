7c574fb73ab2a58c53c2fa1c24c17c74288a7254
ariatmohamed <mohamedhajamer7@gmail.com>
2024-10-27 21:01:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+document.getElementById('submit-btn').addEventListener('click', function() {
+    const videoUrl = document.getElementById('video-url').value;
+    if (videoUrl) {
+        alert(`تم إدخال رابط الفيديو: ${videoUrl}`);
+        // هنا يمكنك إضافة الكود الخاص بتقسيم الفيديو وترجمته
+    } else {
+        alert('يرجى إدخال رابط الفيديو.');
+    }
+});
 
